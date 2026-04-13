552be2cb6251275730518f33c446f23ed7301f4d
Jhaseft <jhasesaat@gmail.com>
2026-04-13 18:34:05 -0400

    version 9.0

--- components/api.js
+++ components/api.js
@@ -1,4 +1,4 @@
 // config/api.js
-const API_BASE_URL = "https://02b6-37-19-200-23.ngrok-free.app"; // Aquí tu backend
+const API_BASE_URL = "https://transfercash.click"; // Aquí tu backend
 
 export default API_BASE_URL;
